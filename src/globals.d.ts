ea0c3ecb47c43dcc504a9885af2ab994b18549ac
Burner <burner@burner.gay>
2022-07-07 09:59:46 -0500

    Initial commit for rewrite

--- src/globals.d.ts
+++
@@ -1,7 +0,0 @@
-// KV stores
-declare const NAME_ALIASES: KVNamespace
-declare const STICKER_ALIASES: KVNamespace
-declare const PACK_METADATA: KVNamespace
-
-// r2 stores
-declare const STICKERS_R2: R2Bucket
